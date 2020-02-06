a197864fcf9ca14f2f1a5b6f0b846a89836d031d
imnottwon <40845175+imnottwon@users.noreply.github.com>
2020-02-06 11:05:57 -0600

    Initial Commit

---
+++ player.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Player struct {
+	fname string
+	lname string
+	plateAppearance int
+	atBat int
+	sinlge int
+	double int
+	triple int
+	homerun int
+	walks int
+	hitByPitch int
+}
+
+func Slugging()
